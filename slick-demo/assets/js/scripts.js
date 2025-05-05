6f06ca00f3d1b1d8a95f685eb0807cb22d2ffda5
Azfar <138599046+Azfar-Sphere@users.noreply.github.com>
2025-05-05 20:56:32 +0000

    fixed slider

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -2,32 +2,31 @@
 
 $(document).ready(function() {
 
-    $('.center').slick({
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
         centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 2,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              dots: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              dots: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+			
 
 });
