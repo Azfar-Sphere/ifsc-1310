ab283f03e9d42c69e9af557af1899b93509a0064
Azfar <138599046+Azfar-Sphere@users.noreply.github.com>
2025-05-05 20:21:30 +0000

    fixed writing info

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -2,16 +2,32 @@
 
 $(document).ready(function() {
 
-    $('.fade').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      slide: 'div',
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 2000
-    });
-
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 2,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              dots: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              dots: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
 
 });
