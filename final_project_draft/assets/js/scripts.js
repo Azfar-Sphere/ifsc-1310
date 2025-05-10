40f8720278af9422965abda281f7f9e3f57991ac
Azfar <138599046+Azfar-Sphere@users.noreply.github.com>
2025-05-10 19:24:07 +0000

    added slick slide

--- final_project_draft/assets/js/scripts.js
+++ final_project_draft/assets/js/scripts.js
@@ -1,29 +1,31 @@
-$(document).ready(function(){
-    $('.mars_pics').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              dots: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              dots: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
+$(document).ready(function() {
+
+  $('.mars_pics').slick({
+    centerMode: true,
+    centerPadding: '40px',
+    slidesToShow: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+        
+  
   });
