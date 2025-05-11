bbee31b11f466af732db34441efbf23f98389175
Azfar <138599046+Azfar-Sphere@users.noreply.github.com>
2025-05-11 11:38:43 +0000

    added mars project

---
+++ mars-rovers/assets/js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  $('.mars_pics').slick({
+    centerMode: true,
+    centerPadding: '40px',
+    slidesToShow: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+        
+  
+  });
